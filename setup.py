f6c064ebcb32040ae9eaf03793b84eb3b32fe7ff
monika-sahay <monika_sahay@yahoo.com>
2024-06-09 19:19:40 +0200

    version 0.0.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ dev_requires = parse_requirements('dev-requirements.txt')
 
 setup(
     name='simplevcs',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
